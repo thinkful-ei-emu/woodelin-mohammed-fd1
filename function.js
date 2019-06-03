b3fdb09ab6e73807bc554145203b4c60263a226e
woodelin <woodelin@woodelins-MacBook-Pro.local>
2019-06-03 16:18:20 -0400

    changed to step two

--- function.js
+++ function.js
@@ -1,1 +1,10 @@
-console.log('Hi my name is woodelin and im 29 years old');
+
+
+function createGreeting(){
+
+    console.log('Hi my name is woodelin and im 29 years old');
+
+
+}
+
+createGreeting();
