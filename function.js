e7a04db1caf562bc0d16ab7b1486778b53ced6e3
woodelin <woodelin@woodelins-MacBook-Pro.local>
2019-06-03 16:21:12 -0400

    added greeting1

--- function.js
+++ function.js
@@ -2,9 +2,10 @@
 
 function createGreeting(){
 
-    console.log('Hi my name is woodelin and im 29 years old');
+    return ('Hi my name is woodelin and im 29 years old');
 
 
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
