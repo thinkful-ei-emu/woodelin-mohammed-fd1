622d610a3e27250ea1cf42e3e927cd812c71002d
woodelin <woodelin@woodelins-MacBook-Pro.local>
2019-06-03 17:00:28 -0400

    stages complete

--- function.js
+++ function.js
@@ -4,12 +4,36 @@ function createGreeting(name,age){
 
     const yob = getYearOfBirth(age);
 
-    return 'I was born in ' + yob;
+    return 'Hi my name is' + name + ' and im ' + age + 'years old. I was born in ' +  yob;
+
+
+    if(name === undefined || age !== 'number'){
+
+         throw new Error('argument is not valid')
+    } 
+
+    if( typeof isNaN(age) || name !== 'string'){
+        throw new Error('age must be a number');
+
+    }
+
+    if( age < 0) {
+        throw new Error('age cannot be a negative');
+    }
+
 
 }
 
-const greeting1 = createGreeting('woodelin', 29);
-console.log(greeting1);
+try {
+
+    const greeting1 = createGreeting('woodelin', 29);
+    console.log(greeting1);
+
+} catch(e){
+    console.e(message);
+}
+
+
 
 function getYearOfBirth(age){
 
